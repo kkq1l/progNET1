b9fa2ab2b32a8d7738bc383eecc52cd5f125d397
Skrybykin <aysen654@mail.ru>
2020-01-28 13:13:41 +0900

    sd

---
+++ ConsoleApplication37/ConsoleApplication37/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication37
+{
+    class Class1
+    {
+        public void Zad8(int day){
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("1: ОС\n2: Дифура\n3: ОС\n4: Англ");
+                    break;
+                case 2:
+                    Console.WriteLine("2: Экономика\n3: Программирование NET");
+                    break;
+                case 3:
+                    Console.WriteLine("2: Физра\n3: Англ");
+                    break;
+                case 4:
+                    Console.WriteLine("1: Теория автоматов и формальных языков\n2: Дифференциальные и разностные уравнения\n3: Теория автоматов и формальных языков");
+                    break;
+                case 5:
+                    Console.WriteLine("1: Структуры и алгоритмы обработки данных\n2: Структуры и алгоритмы обработки данных\n3: Программирование.NET");
+                    break;
+                case 6:
+                    Console.WriteLine("2: Физра\n3: База Данных\n4: База Данных");
+                    break;
+                case 7:
+                    Console.WriteLine("Выходной");
+                    break;
+                default:
+                    Console.WriteLine("Ошибка дня недели");
+                    break;
+            }
+            return;
+        }
+
+        public void Zad7(string name) {
+            char[] firstname = name.ToCharArray();
+            string bukva = Convert.ToString(firstname[firstname.Length-1]);
+            if (bukva == "а")
+            {
+                Console.WriteLine("Здраствуйте госпожа " + name);
+            }
+            else {
+                Console.WriteLine("Здраствуйте господин " + name);
+            }
+            return;
+        }
+
+        public void Zad2(int a, int b) {
+            if (a > b)
+            {
+                Console.WriteLine("Максимальное число равно " + a);
+            }
+            else {
+                Console.WriteLine("Максимальное число равно " + b);
+            }
+            return;
+        }
+    }
+}
---
+++ ConsoleApplication37/ConsoleApplication37/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication37
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            int dd = 1;
+            Class1 asd = new Class1();
+            int zad;
+            string name;
+            int a, b;
+
+            Console.WriteLine("ЛАБАРАТОРНАЯ РАБОТА №1:\n    Выберите номер задачу 1-8:");
+            zad = Convert.ToInt16(Console.ReadLine());
+
+            
+                if (zad == 8)
+                {
+                    Console.WriteLine("Введите день недели для просмотра рассписания:\n             1 - Понедельник;\n             2 - Вторник;\n             3 - Среда и тд");
+
+                    dd = Convert.ToInt16(Console.ReadLine());
+                    asd.Zad8(dd);
+                }
+                else if (zad == 7)
+                {
+                    Console.WriteLine("Введите вашу Фамилию:");
+
+                    name = Console.ReadLine();
+                    asd.Zad7(name);
+                }
+                else if (zad == 2) {
+                    Console.Write("Введите a=");
+
+                    a = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("Введите b=");
+                    b = Convert.ToInt16(Console.ReadLine());
+                    asd.Zad2(a,b);
+                }
+
+            Console.ReadKey();
+            }
+            
+        }
+}
+
